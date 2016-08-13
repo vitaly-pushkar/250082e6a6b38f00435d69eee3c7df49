e7d264a4dc198554f0988d1a7a21aaefbb1ddbd1
Vitaly Pushkar <vitaly.pushkar@gmail.com>
2016-08-13 23:10:11 +0200

    setup rack config to run server

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require 'bundler'
 
 Bundler.require(:default)
 Bundler.require(Sinatra::Base.environment)
+
+require_relative '../app/fyber'
+require_relative '../app/fyber_client'
+require_relative '../app/offers_repository'
