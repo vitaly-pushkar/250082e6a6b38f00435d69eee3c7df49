e39673f315aea06ee1e354e57bb7c084fe0c90b1
Vitaly Pushkar <vitaly.pushkar@gmail.com>
2016-08-13 21:50:16 +0200

    improve repository spec: test empty offers response behaviour

--- spec/app/offers_repository_spec.rb
+++ spec/app/offers_repository_spec.rb
@@ -1,28 +1,42 @@
 require 'spec_helper'
 
 RSpec.describe OffersRepository do
-  let(:offer_1) { {title: 'offer_1', thumbnail: 'offer_1.jpg', payout: 123} }
-  let(:offer_2) { {title: 'offer_2', thumbnail: 'offer_2.jpg', payout: 456} }
+  let(:client) { double('client', call: response) }
+  let(:params) { { uid: 'uid123', pub0: 'campaign1', page: 1 } }
 
-  let(:offers) do
-    [OpenStruct.new(offer_1), OpenStruct.new(offer_2)]
+  context 'response offers not empty' do
+    let(:offer_1) { {title: 'offer_1', thumbnail: 'offer_1.jpg', payout: 123} }
+    let(:offer_2) { {title: 'offer_2', thumbnail: 'offer_2.jpg', payout: 456} }
+
+    let(:offers) do
+      [OpenStruct.new(offer_1), OpenStruct.new(offer_2)]
+    end
+
+    let(:response) do
+      double("response",
+        code: 200,
+        parsed_response:
+          {'offers' => [stringify_keys(offer_1), stringify_keys(offer_2)]}
+      )
+    end
+
+    it 'returns a list of offers' do
+      result = OffersRepository.new(client).get_offers(params)
+
+      expect(result).to eq(offers)
+    end
   end
 
-  let(:response) do
-    double("response",
-      code: 200,
-      parsed_response:
-        {'offers' => [stringify_keys(offer_1), stringify_keys(offer_2)]}
-    )
-  end
+  context 'response offers empty' do
+    let(:response) do
+      double("response", code: 200, parsed_response: {'offers' => []})
+    end
 
-  it 'returns a list of offers' do
-    client = double('client', call: response)
-    params = { uid: 'uid123', pub0: 'campaign1', page: 1 }
+    it 'returns empty list of offers' do
+      result = OffersRepository.new(client).get_offers(params)
 
-    result = OffersRepository.new(client).get_offers(params)
-
-    expect(result).to eq(offers)
+      expect(result).to eq([])
+    end
   end
 
   private
