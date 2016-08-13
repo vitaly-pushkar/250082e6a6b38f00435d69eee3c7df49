1e9ea925cda5831445cb9e42ae3e5371d610a589
Vitaly Pushkar <vitaly.pushkar@gmail.com>
2016-08-13 14:09:52 +0200

    add form to the page

--- app/fyber_api.rb
+++ app/fyber_api.rb
@@ -2,6 +2,12 @@ class FyberApi < Sinatra::Base
   set :views, Proc.new { File.join(root, "views") }
 
   get '/' do
-    slim :index
+    render_page
+  end
+
+  private
+
+  def render_page(offers=[])
+    slim :index, locals: { offers: offers }
   end
 end
--- spec/app/fyber_api_spec.rb
+++ spec/app/fyber_api_spec.rb
@@ -7,8 +7,23 @@ RSpec.describe FyberApi do
 
   context "GET #index" do
     it 'renders index page' do
-      get "/"
+      get '/'
+
       expect(last_response.status).to eq 200
+    end
+
+    it 'renders no offers' do
+      get '/'
+
+      expect(last_response.body).to include('No offers available')
+    end
+
+    it 'renders a form to request offers' do
+      get '/'
+
+      expect(last_response.body).to include('name="uid"')
+      expect(last_response.body).to include('name="pub0"')
+      expect(last_response.body).to include('name="page"')
     end
   end
 end
