0f885e2a68fa54f93f74bad384c728eac021e008
Vitaly Pushkar <vitaly.pushkar@gmail.com>
2016-08-13 22:21:47 +0200

    implement basic api client

--- app/fyber_client.rb
+++ app/fyber_client.rb
@@ -1,4 +1,72 @@
+require 'httparty'
+
+InvalidResponseSignature = Class.new(StandardError)
+
 class FyberClient
-  def get_offers(options = {})
+  include HTTParty
+
+  base_uri 'api.fyber.com'
+
+  def api_key
+    ENV['FYBER_API_KEY'] || 'b07a12df7d52e6c118e5d47d3f9e60135b109a1f'
+  end
+
+  def base_path
+    '/feed/v1/offers.json'
+  end
+
+  def base_params
+    {
+      appid: 157,
+      format: 'json',
+      device_id: '2b6f0cc904d137be2e1730235f5664094b83',
+      ip: '109.235.143.113',
+      locale: 'de',
+      offer_types: 112,
+      uid: 'player1',
+      pub0: 'campaign1',
+      page: 1,
+      timestamp: Time.now.to_i
+    }
+  end
+
+  def call(options = {})
+    params = base_params.merge(options)
+    request_url = generate_request_url(params, api_key)
+
+    response = self.class.get(request_url)
+
+    validate_response_hash(response, api_key)
+  end
+
+  private
+
+  def generate_request_url(params, api_key)
+    query_string = hash_to_query_string(params)
+    hash_key = generate_hash_key(query_string, api_key)
+
+    "#{base_path}?#{query_string}&hashkey=#{hash_key}"
+  end
+
+  def hash_to_query_string(params_hash)
+    params_hash.sort.map{|k,v| "#{k}=#{v}"}.join('&')
+  end
+
+  def generate_hash_key(query_string, api_key)
+    signed_query_string = query_string + "&#{api_key}"
+
+    hash(signed_query_string)
+  end
+
+  def validate_response_hash(response, api_key)
+    signature = response.headers['X-Sponsorpay-Response-Signature']
+    response_hash = hash(response.body + api_key)
+
+    raise InvalidResponseSignature unless signature == response_hash
+    response
+  end
+
+  def hash(string)
+    Digest::SHA1.hexdigest string
   end
 end
---
+++ spec/app/fyber_client_spec.rb
@@ -0,0 +1,116 @@
+require 'spec_helper'
+require 'json'
+
+RSpec.describe FyberClient do
+  let(:api_key) { '123' }
+
+  before do
+    hashkey = Digest::SHA1.hexdigest(response + api_key)
+
+    stub_request(:any, /api.fyber.com/)
+      .to_return(
+        body: response,
+        status: status,
+        headers: { 'X-Sponsorpay-Response-Signature' => hashkey })
+  end
+
+  context 'successful response with no offers' do
+    let(:status) { 200 }
+
+    let(:response) do
+      <<-RESPONSE
+        {
+          "code": "OK",
+          "message": "Ok",
+          "count": 30,
+          "pages": 2,
+          "information": {
+            "app_name": "Demo iframe for publisher - do not touch",
+            "appid": 157,
+            "virtual_currency": "Coins",
+            "virtual_currency_sale_enabled": false,
+            "country": "DE",
+            "language": "DE",
+            "support_url": "http://offer.fyber.com/mobile/support?appid=157&client=api&uid=player1"
+          },
+          "offers": []
+        }
+        RESPONSE
+    end
+
+    it 'returns no offers' do
+      client = FyberClient.new
+      allow(client).to receive(:api_key) { api_key }
+
+      result = client.call
+
+      expect(result.code).to eq(200)
+      expect(result.body).to eq(response)
+      expect(JSON.parse(result.body)['offers']).to be_empty
+    end
+  end
+
+  context 'successful response with offers' do
+    let(:status) { 200 }
+
+    let(:response) do
+      <<-RESPONSE
+      {
+        "code": "OK",
+        "message": "Ok",
+        "count": 30,
+        "pages": 2,
+        "information": {
+          "app_name": "Demo iframe for publisher - do not touch",
+          "appid": 157,
+          "virtual_currency": "Coins",
+          "virtual_currency_sale_enabled": false,
+          "country": "DE",
+          "language": "DE",
+          "support_url": "http://offer.fyber.com/mobile/support?appid=157&client=api&uid=player1"
+        },
+        "offers": [
+          {
+            "title": "Sky",
+            "offer_id": 1030402,
+            "teaser": "Registriere dich mit korrekten Daten.",
+            "required_actions": "Registriere dich mit korrekten Daten.",
+            "link": "http://offer.fyber.com/mobile?impression=true&appid=157&uid=player1&client=api&platform=web&appname=Demo+iframe+for+publisher+-+do+not+touch&traffic_source=offer_api&country_code=DE&pubid=249&ip=109.235.143.113&pub0=campaign1&device_id=2b6f0cc904d137be2e1730235f5664094b83&flash_cookie=e5c83a44cfd62ae87bc18089eddbf366&ad_id=1030402&ad_format=offer&group=Fyber&sig=cd50cb14e67024b882f94593aa9d69b954659aa9",
+            "offer_types": [
+              {
+                "offer_type_id": 105,
+                "readable": "Registrierung"
+              },
+              {
+                "offer_type_id": 112,
+                "readable": "Gratis"
+              }
+            ],
+            "payout": 89454,
+            "time_to_payout": {
+              "amount": 600,
+              "readable": "10 Minuten"
+            },
+            "thumbnail": {
+              "lowres": "http://cdn1.sponsorpay.com/assets/64704/Screen_Shot_2016-08-10_at_3.24.00_PM_square_60.png",
+              "hires": "http://cdn1.sponsorpay.com/assets/64704/Screen_Shot_2016-08-10_at_3.24.00_PM_square_175.png"
+            },
+            "store_id": ""
+          }
+        ]
+      }
+      RESPONSE
+    end
+
+    it 'returns offers' do
+      client = FyberClient.new
+      allow(client).to receive(:api_key) { api_key }
+
+      result = client.call
+
+      expect(result.code).to eq(200)
+      expect(result.body).to eq(response)
+      expect(JSON.parse(result.body)['offers']).not_to be_empty
+    end
+  end
+end
